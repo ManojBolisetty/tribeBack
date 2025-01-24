9b29e1c738321aa6d501f8d19154ec7abe1949c6
ManojBolisetty <manojbolisetty11@gmail.com>
2025-01-24 22:40:48 +0530

    chore: test email api

--- src/routes/email/index.ts
+++ src/routes/email/index.ts
@@ -24,5 +24,10 @@ router.post("/send_email", async (req: any, res: any) => {
     res.status(500).json(result);
   }
 });
+router.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to the Email API!",
+  });
+});
 
 export default router;
